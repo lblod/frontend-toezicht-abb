22d72a52c7d9592f289df3b956edfd8f82724654
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-08 19:11:41 +0430

    Move a computed property to the model

--- app/models/besluit-type.js
+++ app/models/besluit-type.js
@@ -1,9 +1,14 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
+import { computed }  from '@ember/object';
 
 export default Model.extend({
   label: attr(),
   decidableBy: hasMany('bestuurseenheid-classificatie-code', { inverse: null }),
-  uri: attr()
+  uri: attr(),
+
+  isRegulation: computed('uri', function(){
+    return this.uri === 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
+  })
 });
--- app/models/inzending-voor-toezicht.js
+++ app/models/inzending-voor-toezicht.js
@@ -42,6 +42,6 @@ export default Model.extend({
   fileAddresses: hasMany('file-address', { inverse: null}),
 
   isRegulation: computed(function(){
-    return this.get('besluitType.uri') === 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
+    return this.get('besluitType.isRegulation');
   })
 });
