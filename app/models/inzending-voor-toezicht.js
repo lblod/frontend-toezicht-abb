ce78e7d7b0d99b1e369d59b1cfa9adf6feaef77d
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-08 18:10:50 +0430

    Check uri rather than id

--- app/models/inzending-voor-toezicht.js
+++ app/models/inzending-voor-toezicht.js
@@ -1,6 +1,7 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
+import { computed }  from '@ember/object';
 
 export default Model.extend({
   created: attr('datetime'),
@@ -38,5 +39,9 @@ export default Model.extend({
   files: hasMany('file', { inverse: null }),
   taxRates: hasMany('tax-rate', { inverse: null }),
   melding: belongsTo('inzending-voor-toezicht-melding', { inverse: 'inzendingVoorToezicht' }),
-  fileAddresses: hasMany('file-address', { inverse: null})
+  fileAddresses: hasMany('file-address', { inverse: null}),
+
+  isRegulation: computed(function(){
+    return this.get('besluitType.uri') === 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
+  })
 });
