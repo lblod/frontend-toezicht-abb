fdafaa0f6d8a5d09c0bcbba4551e14498ee7d731
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-08 16:31:49 +0430

    Fetch uri of besluitType resources

--- app/models/besluit-type.js
+++ app/models/besluit-type.js
@@ -4,5 +4,6 @@ import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   label: attr(),
-  decidableBy: hasMany('bestuurseenheid-classificatie-code', { inverse: null })
+  decidableBy: hasMany('bestuurseenheid-classificatie-code', { inverse: null }),
+  uri: attr()
 });
