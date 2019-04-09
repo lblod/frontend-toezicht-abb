7e8a5d324ea933753d158161f328f91ca81bb68e
Mehran Naghizadeh <tarhineh@gmail.com>
2019-04-09 21:03:21 +0430

    Use equal rather than computed to shorten the code

--- app/models/besluit-type.js
+++ app/models/besluit-type.js
@@ -7,8 +7,5 @@ export default Model.extend({
   label: attr(),
   decidableBy: hasMany('bestuurseenheid-classificatie-code', { inverse: null }),
   uri: attr(),
-
-  isRegulation: computed('uri', function(){
-    return this.uri === 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03';
-  })
+  isRegulation: equal('uri', 'http://data.lblod.info/DecisionType/5b3955cc006323233e711c482f3a6bf39a8d3eba6bbdb2c672bdfcf2b2985b03')
 });
