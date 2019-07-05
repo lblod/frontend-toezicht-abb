c6ff981d6a5221493b25f232de1991ca23ef13e1
Claire Lovisa <lovisa.claire@gmail.com>
2019-07-05 15:13:55 +0200

    Run codemods

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
+import { underscore } from '@ember/string';
+
 
 export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
 
