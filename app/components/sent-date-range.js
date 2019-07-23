bde85a8bd4a050d78326b3bfc4fc62a4966ae71b
Claire Lovisa <lovisa.claire@gmail.com>
2019-07-23 09:43:39 +0200

    Use data down actions up for date pickers and remove observers

--- app/components/sent-date-range.js
+++ app/components/sent-date-range.js
@@ -1,59 +1,54 @@
 import Component from '@ember/component';
 import moment from 'moment';
-import { computed, observer } from '@ember/object';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   classNames: ['js-accordion', 'js-accordion--open'],
   label: 'Periode verstuurd',
 
-  _fromDate: null,  // date object
-  _toDate: null,  // date object
-
   fromValue: null,  // ISO string
   toValue: null,  // ISO string
+
+  fromDate: computed('fromValue', function() {
+    try {
+      return new Date(Date.parse(this.fromValue));
+    } catch(e) {
+      return null;
+    }
+  }),
+
+  toDate: computed('toValue', function() {
+    try {
+      return new Date(Date.parse(this.toValue));
+    } catch(e) {
+      return null;
+    }
+  }),
 
   isFilterEnabled: computed('fromValue', 'toValue', function() {
     return this.fromValue || this.toValue;
   }),
 
-  async didReceiveAttrs() {
-    this._super(...arguments);
-    if (this.fromValue && !this._fromDate) {
-      const date = new Date(Date.parse(this.fromValue));
-      this.set('_fromDate', date);
-    } else if (!this.fromValue) {
-      this.set('_fromDate', null);
-    }
-
-    if (this.toValue && !this._toDate) {
-      const date = new Date(Date.parse(this.toValue));
-      this.set('_toDate', date);
-    } else if (!this.toValue) {
-      this.set('_toDate', null);
-    }
-  },
-
-  // TODO: Remove observer when the wu-date-range component supports data down actions up
-  fromDateChanged: observer('_fromDate', function() { // eslint-disable-line ember/no-observers
-      if (this._fromDate)
-      this.set('fromValue', this._fromDate.toISOString());
-  }),
-
-  // TODO: Remove observer when the wu-date-range component supports data down actions up
-  toDateChanged: observer('_toDate', function() { // eslint-disable-line ember/no-observers
-    if (this._toDate)
-      this.set('toValue', this._toDate.toISOString());
-  }),
-
   actions: {
     resetFilter() {
-      ['fromValue', 'toValue'].forEach(filter => this.set(filter, null));
+      this.onChangeFromValue(null);
+      this.onChangeToValue(null);
     },
+
     initRangeFilter() {
       const yesterday = moment().subtract(1, 'day').startOf('day');
       const today = moment().endOf('day');
-      this.set('_fromDate', yesterday.toDate());
-      this.set('_toDate', today.toDate());
+      this.onChangeFromValue(yesterday.toDate().toISOString());
+      this.onChangeToValue(today.toDate().toISOString());
+    },
+
+    updateDate(varName, date) {
+      const dateString = date.toISOString();
+      if (varName == 'fromDate') {
+        this.onChangeFromValue(dateString);
+      } else {
+        this.onChangeToValue(dateString);
+      }
     }
   }
 });
