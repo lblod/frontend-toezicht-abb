b6eabe884b03326c5fa4cb184b31355c2d025279
Claire Lovisa <lovisa.claire@gmail.com>
2019-12-10 17:16:40 +0100

    Fix redirection to show route

--- app/routes/toezicht.js
+++ app/routes/toezicht.js
@@ -4,12 +4,26 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   currentSession: service(),
+  router: service(),
 
-  redirect() {
+  redirect(model, transition) {
+    const targetName = transition.targetName;
     if (this.currentSession.canReadVlabel) {
-      this.transitionTo('toezicht.vlabel-inzendingen.index');
+      // if the target route is in the vlabel domain we transition to it,
+      // otherwise as we have the vlabel rights we redirect to the vlabel index.
+      if (targetName.split('.')[1] == 'vlabel-inzendingen') {
+        this.transitionTo(targetName);
+      } else {
+        this.transitionTo('toezicht.vlabel-inzendingen');
+      }
     } else {
-      this.transitionTo('toezicht.inzendingen.index');
+      // if the target route is in the "regular" domain we transition to it,
+      // otherwise as we have the "regular" rights we redirect to the "regular" index.
+      if (targetName.split('.')[1] == 'inzendingen') {
+        this.transitionTo(targetName);
+      } else {
+        this.transitionTo('toezicht.inzendingen');
+      }
     }
   }
 });
