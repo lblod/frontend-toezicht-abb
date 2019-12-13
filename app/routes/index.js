943e19a60913e506175de839f14b83eb8a7cd9e3
Erika Pauwels <erika.pauwels@gmail.com>
2019-12-13 15:10:10 +0100

    Fix missing service injection

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  currentSession: service(),
+
   beforeModel() {
     if (this.currentSession.canReadVlabel) {
       this.transitionTo('toezicht.vlabel-inzendingen.index');
