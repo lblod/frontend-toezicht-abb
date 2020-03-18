d1af4480a93090bae3dcdd16f1ee1a5724a56f91
Claire Lovisa <lovisa.claire@gmail.com>
2020-03-18 10:12:07 +0100

    Remove @classic decorator on services

--- app/services/current-session.js
+++ app/services/current-session.js
@@ -1,27 +1,42 @@
-import classic from 'ember-classic-decorator';
 import Service, { inject as service } from '@ember/service';
-import { get, computed } from '@ember/object';
+import { get } from '@ember/object';
 import { task, waitForProperty } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
-@classic
 export default class CurrentSessionService extends Service {
-  @service('session')
-  session;
+  @service('session') session;
+  @service('store') store;
 
-  @service('store')
-  store;
+  @tracked _account;
+  @tracked _user;
+  @tracked _group;
+
+  // this is a promise
+  get account() {
+    return this.makePropertyPromise.perform('_account');
+  }
+
+  // this contains a promise
+  get user() {
+    return this.makePropertyPromise.perform('_user');
+  }
+
+  // this contains a promise
+  get group() {
+    return this.makePropertyPromise.perform('_group');
+  }
 
   async load() {
-    if (this.get('session.isAuthenticated')) {
+    if (this.session.isAuthenticated) {
       const session = this.session;
       const account = await this.store.find('account', get(session, 'data.authenticated.relationships.account.data.id'));
-      const user = await account.get('gebruiker');
+      const user = await account.gebruiker;
       const group = await this.store.find('bestuurseenheid', get(session, 'data.authenticated.relationships.group.data.id'));
       const roles = await get(session, 'data.authenticated.data.attributes.roles');
-      this.set('_account', account);
-      this.set('_user', user);
-      this.set('_roles', roles);
-      this.set('_group', group);
+      this._account = account;
+      this._user = user;
+      this._roles = roles;
+      this._group = group;
 
       // The naming is off, but account,user,roles are taken for the
       // promises in a currently public API.
@@ -32,9 +47,9 @@ export default class CurrentSessionService extends Service {
         groupContent: group
       });
 
-      this.set('canReadVlabel', this.canAccess('ABBDatabankToezicht-DatabankToezichtVLABEL'));
-      this.set('canRead', this.canAccess('ABBDatabankToezicht-DatabankToezichtLezer') || this.canAccess('ABBDatabankToezicht-DatabankToezichtEditeur'));
-      this.set('canWrite', this.canAccess('ABBDatabankToezicht-DatabankToezichtEditeur'));
+      this.canReadVlabel = this.canAccess('ABBDatabankToezicht-DatabankToezichtVLABEL');
+      this.canRead = this.canAccess('ABBDatabankToezicht-DatabankToezichtLezer') || this.canAccess('ABBDatabankToezicht-DatabankToezichtEditeur');
+      this.canWrite = this.canAccess('ABBDatabankToezicht-DatabankToezichtEditeur');
     }
   }
 
@@ -45,25 +60,7 @@ export default class CurrentSessionService extends Service {
   // constructs a task which resolves in the promise
   @task(function * (property) {
     yield waitForProperty(this, property);
-    return this.get(property);
+    return this.property;
   })
   makePropertyPromise;
-
-  // this is a promise
-  @computed('_account')
-  get account() {
-    return this.makePropertyPromise.perform('_account');
-  }
-
-  // this contains a promise
-  @computed('_user')
-  get user() {
-    return this.makePropertyPromise.perform('_user');
-  }
-
-  // this contains a promise
-  @computed('_group')
-  get group() {
-    return this.makePropertyPromise.perform('_group');
-  }
 }
