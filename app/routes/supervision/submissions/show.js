3028164ba4466614b4995cd34b1d106e44266245
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-17 10:47:31 +0200

    first working show with submission form.

--- app/routes/supervision/submissions/show.js
+++ app/routes/supervision/submissions/show.js
@@ -2,4 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class SupervisionSubmissionsShowRoute extends Route {
 
+  async model(params) {
+    return await this.store.find('submission', params.id);
+  }
 }
