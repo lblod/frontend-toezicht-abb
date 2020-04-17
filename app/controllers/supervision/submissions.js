fdce2295f902aa498bc42fcb71eca475f2a559ed
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-17 15:18:32 +0200

    first effort at trying to integrate vlabel table.

--- app/controllers/supervision/submissions.js
+++ app/controllers/supervision/submissions.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
+import ENV from 'frontend-toezicht-abb/config/environment';
+
 export default class SupervisionSubmissionsController extends Controller {
   @service router;
   @service currentSession;
@@ -8,6 +10,11 @@ export default class SupervisionSubmissionsController extends Controller {
   page = 0;
   size = 20;
   sort = '-sent-date';
+
+  constructor() {
+    super(...arguments);
+    this.set('header', ENV['vo-webuniversum']['header']);
+  }
 
   get hasActiveChildRoute() {
       return this.router.currentRouteName.startsWith('supervision.submissions')
--- app/routes/supervision/submissions.js
+++ app/routes/supervision/submissions.js
@@ -1,11 +1,62 @@
 import Route from '@ember/routing/route';
+import {inject as service} from '@ember/service';
+
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
+import config from '../../config/environment';
+import Snapshot from "../../utils/snapshot";
+
+// TODO do selective filtering based on the model, based on if the user is a Vlabel User
 export default class SupervisionSubmissionsRoute extends Route.extend(DataTableRouteMixin) {
-  modelName = 'Submission';
+  @service currentSession;
+
+  modelName = 'submission';
+
   queryParams = {
     page: {refreshModel: true},
     size: {refreshModel: true},
     sort: {refreshModel: true}
   };
+
+  constructor() {
+    super(...arguments);
+    this.lastParams = new Snapshot();
+  }
+
+  mergeQueryOptions(params) {
+    this.lastParams.stageLive(params);
+
+    const filterParams = [
+      'size',
+      'sort'
+    ];
+
+    if (this.lastParams.anyFieldChanged(filterParams))
+      params.page = 0;
+
+    let query;
+    if (this.currentSession.canReadVlabel) {
+      // TODO correct inclusions and filtering
+      query = {
+        page: {number: params.page}
+      }
+      // query['filter[besluit-type][:uri:]'] = config.besluitTypeUri;
+      // query['filter[form-data][types][:uri:]'] = config.regulationTypeUri;
+      // query['filter[form-data][tax-type][:uri:]'] = config.taxTypeUri;
+      // query['filter[form-data][chart-of-account][id]'] = config.marCodes.join(',');
+    } else {
+      query = {
+        page: {number: params.page}
+      }
+    }
+
+    this.lastParams.commit();
+    return query;
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    if (controller.page !== this.lastParams.committed.page)
+      controller.set('page', this.lastParams.committed.page);
+  }
 }
