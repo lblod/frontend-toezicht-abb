78e0bc24ece5b8b69588be1487d191f6d4a4389f
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-20 14:22:50 +0200

    refactored administrative-unit-classification-select to use concepts.

--- app/components/filter/administrative-unit-classification-select.js
+++ app/components/filter/administrative-unit-classification-select.js
@@ -1,9 +1,11 @@
-import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
+import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { timeout } from 'ember-concurrency';
-import { task, restartableTask } from 'ember-concurrency-decorators';
+import {tracked} from '@glimmer/tracking';
+import {timeout} from 'ember-concurrency';
+import {task, restartableTask} from 'ember-concurrency-decorators';
+
+const ADMIN_UNIT_CLASS_CONCEPT_SCHEME = 'http://data.vlaanderen.be/id/conceptscheme/BestuurseenheidClassificatieCode';
 
 export default class AdministrativeUnitClassificationSelectComponent extends Component {
   @service store
@@ -17,8 +19,13 @@ export default class AdministrativeUnitClassificationSelectComponent extends Com
   }
 
   @task
-  *loadData() {
-    const options = yield this.store.query('bestuurseenheid-classificatie-code', {
+  * loadData() {
+    const options = yield this.store.query('concept', {
+      filter: {
+        "concept-schemes": {
+          ":uri:": ADMIN_UNIT_CLASS_CONCEPT_SCHEME
+        }
+      },
       sort: 'label'
     });
     this.options = options;
@@ -27,10 +34,15 @@ export default class AdministrativeUnitClassificationSelectComponent extends Com
   }
 
   @restartableTask
-  *search (term) {
+  * search(term) {
     yield timeout(600);
-    return this.store.query('bestuurseenheid-classificatie-code', {
-      filter: { label: term }
+    return this.store.query('concept', {
+      filter: {
+        label: term,
+        "concept-schemes": {
+          ":uri:": ADMIN_UNIT_CLASS_CONCEPT_SCHEME
+        }
+      }
     });
   }
 
@@ -43,9 +55,14 @@ export default class AdministrativeUnitClassificationSelectComponent extends Com
   @action
   async updateSelectedValue() {
     if (this.args.value && !this.selected) {
-      this.selected = await this.store.query('bestuurseenheid-classificatie-code', {
-        filter: { id: this.args.value },
-        page: { size: this.args.value.split(',').length}
+      this.selected = await this.store.query('concept', {
+        filter: {
+          id: this.args.value,
+          "concept-schemes": {
+            ":uri:": ADMIN_UNIT_CLASS_CONCEPT_SCHEME
+          }
+        },
+        page: {size: this.args.value.split(',').length}
       });
     } else if (!this.args.value) {
       this.selected = null;
