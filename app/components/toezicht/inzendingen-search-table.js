1757a989af3626e8413e060573a836a05773c67a
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-21 17:50:24 +0200

    Remove old search table

--- app/components/toezicht/inzendingen-search-table.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class InzendingenSearchTableComponent extends Component {
-  get target() {
-    return this.args.targetRoute || "toezicht.inzendingen.show";
-  }
-}
