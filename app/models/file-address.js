b30bde56b16bb03b8c315d27acc1fe983252c041
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-27 15:08:45 +0200

    removed deprecated models and inflector rules.

--- app/models/file-address.js
+++
@@ -1,7 +0,0 @@
-import Model, { belongsTo, attr } from '@ember-data/model';
-
-export default class FileAddress extends Model {
-  @attr() address;
-
-  @belongsTo('file', { inverse: null }) replicatedFile;
-}
--- app/models/inzending-voor-toezicht-melding.js
+++
@@ -1,8 +0,0 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
-
-export default class InzendingVoorToezichtMelding extends Model {
-  @attr() description;
-
-  @belongsTo('melding-status', { inverse: null }) status;
-  @belongsTo('inzending-voor-toezicht', { inverse: null }) inzendingVoorToezicht;
-}
