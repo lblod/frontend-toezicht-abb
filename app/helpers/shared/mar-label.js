c1deb6b23825f1f2c110e13d5cd8e7bef0eb0af2
Dries Hugaerts <dries.hugaerts@live.be>
2020-04-29 12:06:53 +0200

    fixed vlabel-tabel notation bug

--- app/helpers/shared/mar-label.js
+++
@@ -1,5 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function sharedMarLabel([concept]/*, hash*/) {
-  return `MAR${concept.notation} - ${concept.label}`;
-});
--- app/models/chart-of-account.js
+++ app/models/chart-of-account.js
@@ -1,5 +1,6 @@
 import Model, {attr} from '@ember-data/model';
 
 export default class ChartOfAccountModel extends Model {
-  @attr label
+  @attr label;
+  @attr notation;
 }
