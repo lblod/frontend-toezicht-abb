ca91034c65e772a8333b0dea374c055c285b20e9
Miet Claes <github@miet.be>
2020-06-08 13:10:15 +0200

    Remove header configuration and make session Authenticated available

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,6 @@
 import Controller from '@ember/controller';
-import ENV from 'frontend-toezicht-abb/config/environment';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
-  constructor() {
-    super(...arguments);
-    this.header = ENV['vo-webuniversum']['header'];
-  }
+  @service session;
 }
