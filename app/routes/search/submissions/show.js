ddd28d73b38ecee6e20561f3130a1e9d4fb274f1
Dries Hugaerts <dries.hugaerts@live.be>
2020-06-12 15:26:34 +0200

    improved model fetch on detail views.

--- app/routes/search/submissions/show.js
+++ app/routes/search/submissions/show.js
@@ -2,6 +2,14 @@ import Route from '@ember/routing/route';
 
 export default class SearchSubmissionsShowRoute extends Route {
   model(params) {
-    return this.store.find('submission', params.id);
+    return this.store.findRecord('submission', params.id, {
+      include:[
+        'organization.classificatie',
+        'organization.provincie',
+        'review.status',
+        'form-data.types',
+        'last-modifier',
+      ].join(',')
+    });
   }
 }
