c746bf9d8ddceadbea2f78ff3c0a4450b6cfb873
Miet <github@miet.be>
2020-06-15 18:42:47 +0200

    Add modal frame for searches and modal with content for filters

--- app/router.js
+++ app/router.js
@@ -40,4 +40,9 @@ Router.map(function() {
     this.route('filter-save');
   });
   this.route('help');
+
+  this.route('filters-table', function() {
+    this.route('filter-overview');
+    this.route('table-config');
+  });
 });
