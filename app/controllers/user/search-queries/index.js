fd9cd3f50093e48f1e5cc17f49762e830d9eb9cd
Dries Hugaerts <dries.hugaerts@live.be>
2020-06-24 16:48:07 +0200

    added delete option on index page of search-queries

---
+++ app/controllers/user/search-queries/index.js
@@ -0,0 +1,18 @@
+import Controller from '@ember/controller';
+
+import fetch from 'node-fetch';
+
+import { task } from 'ember-concurrency-decorators';
+
+export default class UserSearchQueriesIndexController extends Controller {
+  page = 0
+  size = 20
+
+  @task
+  *delete(id) {
+    yield fetch(`/search-queries/${id}`, {
+      method: 'DELETE',
+    });
+    yield this.model.update();
+  }
+}
