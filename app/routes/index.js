6036f9da0e7662354d36e46bee3dc7d4b2b1919f
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-06-29 19:40:18 +0200

    Make sure the index route uses the AuthenticatedRouteMixin

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,17 @@
 import Route from '@ember/routing/route';
 import {inject as service} from '@ember/service';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class IndexRoute extends Route {
+export default class IndexRoute extends Route.extend(AuthenticatedRouteMixin) {
   @service currentSession;
 
-  beforeModel() {
+  beforeModel(transition) {
     if (this.currentSession.canWrite || this.currentSession.canReadVlabel) {
       this.transitionTo('supervision.submissions');
-    } else {
+    }
+    else {
       this.transitionTo('search.submissions');
     }
+    super.beforeModel(...arguments);
   }
 }
