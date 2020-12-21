8b44a59995c91833fac76a7c33cd191dc145d418
achraf <achrafatauil@hotmail.com>
2020-12-21 15:36:42 +0100

    run  `ember-cli-update --to 3.20`

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'frontend-toezicht-abb/app';
+import config from 'frontend-toezicht-abb/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
