734a0bdc101754dd176726f70f29987ba2d6df4f
Wout Schellaert <wout.schellaert@gmail.com>
2021-04-09 15:10:13 +0200

    Fix #11
    If there is no base yet (e.g. on page refresh), all fields will always be changed, but we want to respect the page parameter anyway.

--- app/routes/search/submissions.js
+++ app/routes/search/submissions.js
@@ -28,7 +28,7 @@ export default class SearchSubmissionsRoute extends Route {
     this.filter = params;
     this.lastParams.stageLive(params);
 
-    if (this.lastParams.anyFieldChanged(Object.keys(params).filter(key => key !== 'page'))) {
+    if (this.lastParams.hasBase && this.lastParams.anyFieldChanged(Object.keys(params).filter(key => key !== 'page'))) {
       params.page = 0;
     }
 
