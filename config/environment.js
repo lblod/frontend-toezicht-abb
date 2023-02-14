2599cf586c4ce86eb935815f187befb6f477dd54
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-02-14 17:35:15 +0100

    Update to Ember 4.10

--- config/environment.js
+++ config/environment.js
@@ -1,16 +1,12 @@
 'use strict';
 
 module.exports = function (environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'frontend-toezicht-abb',
     environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'history',
     EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-      },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false,
