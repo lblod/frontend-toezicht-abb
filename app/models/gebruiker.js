924efe274545e6da9674face962c2c6078c9977b
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-02-15 17:02:37 +0100

    Don't use `firstObject`
    Accessing non-array properties on RecordArrays is deprecated.

--- app/models/gebruiker.js
+++ app/models/gebruiker.js
@@ -2,13 +2,15 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class Gebruiker extends Model {
   @attr uri;
+  @attr voornaam;
+  @attr achternaam;
 
   @hasMany('account', { async: true, inverse: null }) account;
   @hasMany('bestuurseenheid', { async: true, inverse: null }) bestuurseenheden;
   @hasMany('search-query', { async: true, inverse: 'user' }) searchQueries;
 
   get group() {
-    return this.hasMany('bestuurseenheden').value().firstObject;
+    return this.hasMany('bestuurseenheden').value()[0];
   }
 
   get fullName() {
