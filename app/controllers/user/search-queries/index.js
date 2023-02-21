9aefeb5e8dcebb7ff8854ce6d4650ec6fbac6653
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-02-21 10:50:40 +0100

    Refresh the model when deleting search queries
    `this.model.update` no longer works or doesn't trigger a rerender. We can work around this by simply reloading the model hook. Ideally we can use `record.destroyRecord()` but that requires backend changes.

--- app/controllers/user/search-queries/index.js
+++ app/controllers/user/search-queries/index.js
@@ -1,16 +1,20 @@
 import Controller from '@ember/controller';
+import { service } from '@ember/service';
 
 import { task } from 'ember-concurrency';
 import { removeSourceData } from '../../../utils/filter-form-helpers';
 
 export default class UserSearchQueriesIndexController extends Controller {
+  @service router;
+
   page = 0;
   size = 10;
   sort = '-created';
 
   @task
   *delete(id) {
+    // TODO: The backend needs to be updated so Ember Data's `record.destroyRecord()` method works and we don't need the route refreshing code anymore
     yield removeSourceData(`/search-queries/${id}`);
-    yield this.model.update();
+    this.router.refresh('user.search-queries.index');
   }
 }
