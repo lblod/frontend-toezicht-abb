82ebd6518cab40b0dfd965f725dd26d5778a9835
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-03-18 13:32:54 +0100

    Resolve some EmberData deprecations

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,8 +2,15 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
   const app = new EmberApp(defaults, {});
+
+  const { setConfig } = await import('@warp-drive/build-config');
+  setConfig(app, __dirname, {
+    deprecations: {
+      DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+    },
+  });
 
   return app.toTree();
 };
