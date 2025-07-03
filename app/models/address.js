a41e5277024279984fc15c3807b96354f511713a
Kobe Mertens <kobe.mertens@hotmail.com>
2025-07-03 17:10:54 +0200

    add minimal model changes

---
+++ app/models/address.js
@@ -0,0 +1,11 @@
+import Model, { belongsTo } from '@ember-data/model';
+
+export default class AddressModel extends Model {
+
+  @belongsTo('concept', {
+    inverse: null,
+    async: true,
+  })
+  provincie;
+
+}
