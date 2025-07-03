a6ed37613780b3040770dfb85f28ebffbd0c1cca
Kobe Mertens <kobe.mertens@hotmail.com>
2025-07-03 17:10:27 +0200

    add inflector rules for dutch-english translation

--- app/config/custom-inflector-rules.js
+++ app/config/custom-inflector-rules.js
@@ -6,3 +6,5 @@ irregular('conversatie', 'conversaties');
 uncountable('form-data');
 irregular('inzending-voor-toezicht', 'inzendingen-voor-toezicht');
 irregular('werkingsgebied', 'werkingsgebieden');
+irregular('address', 'adressen');
+irregular('site', 'vestigingen')
