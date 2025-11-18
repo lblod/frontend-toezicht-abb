f3e7c519da80b91e83b7e171670c56944bef873e
Kobe Mertens <kobe.mertens@hotmail.com>
2025-11-18 15:13:41 +0100

    add kbo number to `administrative-unit-select`

--- app/models/bestuurseenheid.js
+++ app/models/bestuurseenheid.js
@@ -2,6 +2,7 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class Bestuurseenheid extends Model {
   @attr naam;
+  @attr kboNumber;
   @attr('string-set') alternatieveNaam;
 
   @belongsTo('werkingsgebied', { async: true, inverse: null }) werkingsgebied;
