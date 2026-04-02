5a2c47303c0bdadad7362fd6d270c5dd6490a4b4
Kike Franssen <114907819+kikef98@users.noreply.github.com>
2026-04-02 14:19:57 +0200

    DL-7236:  Adjust front-end for new URL-formatting in Databank Toezicht (#78)

--- app/routes/supervision/submissions/show.js
+++ app/routes/supervision/submissions/show.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { registerFormField } from 'frontend-toezicht-abb/components/submissions/fields/decision-articles-field';
 import { registerFormField as registerDecisionDocumentsField } from 'frontend-toezicht-abb/components/submissions/fields/decision-documents-field';
+import { registerFormField as registerDecisionRemoteDocumentsField } from 'frontend-toezicht-abb/components/submissions/fields/decision-remote-documents';
 
 export default class SupervisionSubmissionsShowRoute extends Route {
   @service store;
@@ -11,6 +12,7 @@ export default class SupervisionSubmissionsShowRoute extends Route {
 
     registerFormField();
     registerDecisionDocumentsField();
+    registerDecisionRemoteDocumentsField();
   }
 
   model(params) {
